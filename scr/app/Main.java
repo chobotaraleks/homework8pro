5cbe80dbd5b88f0bbecf5a5acb697b9707dae686
chobotaraleks <chobotaraleks@gmail.com>
2024-11-28 18:25:49 +0200

    Initial commit: Validate funds functionality

---
+++ scr/app/Main.java
@@ -0,0 +1,42 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static double balance;
+
+    public static void main(String[] args) {
+        balance = getBalance();
+        validateAmount(balance, getAmount());
+    }
+
+    private static double getBalance() {
+        return 1000.00; // Наявні кошти на рахунку
+    }
+
+    private static double getAmount() {
+        System.out.printf("Balance is USD %.2f.%nEnter purchase amount, USD: ", balance);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
+    }
+
+    // Метод валідації наявних коштів
+    private static void validateAmount(double balance, double withdrawal) {
+        if (withdrawal > balance) {
+            try {
+                throw new FundsException("Insufficient funds!");
+            } catch (FundsException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } else {
+            balance = getBalanceAfterPurchase(balance, withdrawal);
+            System.out.printf("Funds are OK. Purchase paid.%nBalance is USD %.2f", balance);
+        }
+    }
+
+    // Метод розрахунку наявних коштів після зняття певної суми
+    private static double getBalanceAfterPurchase(double balance, double withdrawal) {
+        return balance - withdrawal;
+    }
+}
